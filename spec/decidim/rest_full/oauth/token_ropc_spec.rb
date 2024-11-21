c52e85cbca04e742b70221e83f44fce9c16dd396
Hadrien Froger <hadrien@octree.ch>
2024-11-21 23:58:58 +0000

    :sparkles: (token) Can register the user on the fly while impersonating

---
+++ spec/decidim/rest_full/oauth/token_ropc_spec.rb
@@ -0,0 +1,252 @@
+# frozen_string_literal: true
+
+# spec/integration/oauth_scopes_spec.rb
+require "swagger_helper"
+RSpec.describe "Decidim::Api::RestFull::System::ApplicationController", type: :request do
+  let!(:organization) { create(:organization) }
+  let!(:user) { create(:user, organization: organization, password: "decidim123456789!", password_confirmation: "decidim123456789!") }
+  let!(:api_client) { create(:api_client, organization: organization) }
+
+  before do
+    host! organization.host
+  end
+
+  path "/oauth/token" do
+    post "Request a OAuth token throught ROPC" do
+      tags "OAuth"
+      consumes "application/json"
+      produces "application/json"
+      security([])
+      parameter name: :body, in: :body, required: true, schema: { "$ref" => "#/components/schemas/oauth_grant_param" }
+      response "200", "Token returned" do
+        context "when user does not exists" do
+          context "with meta.register_on_missing=true" do
+            let(:nickname) do
+              final_nickname = nickname = "decidimuser"
+              i = 0
+              while Decidim::User.find_by(nickname: final_nickname)
+                i += 1
+                final_nickname = "#{nickname}_#{i}"
+              end
+              final_nickname
+            end
+            let(:body) do
+              {
+                grant_type: "password",
+                auth_type: "impersonate",
+                username: nickname,
+                client_id: api_client.client_id,
+                client_secret: api_client.client_secret,
+                meta: {
+                  register_on_missing: true
+                },
+                scope: "public"
+              }
+            end
+
+            run_test! do |_example|
+              expect(Decidim::User.find_by(nickname: nickname)).to be_truthy
+            end
+          end
+
+          context "with meta.register_on_missing=true and extra.email=an_email" do
+            let(:nickname) do
+              final_nickname = nickname = "decidimuser"
+              i = 0
+              while Decidim::User.find_by(nickname: final_nickname)
+                i += 1
+                final_nickname = "#{nickname}_#{i}"
+              end
+              final_nickname
+            end
+            let(:email) do
+              "test#{Devise.friendly_token.first(10).downcase}@example.org"
+            end
+            let(:body) do
+              {
+                grant_type: "password",
+                auth_type: "impersonate",
+                username: nickname,
+                client_id: api_client.client_id,
+                client_secret: api_client.client_secret,
+                meta: {
+                  register_on_missing: true
+                },
+                extra: {
+                  email: email
+                },
+                scope: "public"
+              }
+            end
+
+            run_test! do |_example|
+              expect(Decidim::User.find_by(nickname: nickname).email).to eq(email)
+            end
+          end
+        end
+
+        context "with grant=password auth_type=impersonate" do
+          let(:body) do
+            {
+              grant_type: "password",
+              auth_type: "impersonate",
+              username: user.nickname,
+              client_id: api_client.client_id,
+              client_secret: api_client.client_secret,
+              scope: "public"
+            }
+          end
+
+          run_test!(example_name: :ok_ropc_impersonate) do |response|
+            json_response = JSON.parse(response.body)
+            expect(json_response["access_token"]).to be_present
+            expect(
+              Doorkeeper::AccessToken.find_by(token: json_response["access_token"]).scopes
+            ).to include("public")
+          end
+        end
+
+        context "with grant=password auth_type=login" do
+          let(:proposal_api_client) { create(:api_client, organization: organization, scopes: "proposals public") }
+
+          let(:body) do
+            {
+              grant_type: "password",
+              auth_type: "login",
+              password: "decidim123456789!",
+              username: user.nickname,
+              client_id: proposal_api_client.client_id,
+              client_secret: proposal_api_client.client_secret,
+              scope: "public proposals"
+            }
+          end
+
+          run_test!(example_name: :ok_ropc_login) do |response|
+            json_response = JSON.parse(response.body)
+            expect(json_response["access_token"]).to be_present
+            expect(
+              Doorkeeper::AccessToken.find_by(token: json_response["access_token"]).scopes
+            ).to include("proposals")
+          end
+        end
+      end
+
+      response "400", "Bad Request" do
+        context "when user does not exists" do
+          context "with meta.register_on_missing=false" do
+            let(:nickname) do
+              final_nickname = nickname = "decidimuser"
+              i = 0
+              while Decidim::User.find_by(nickname: final_nickname)
+                i += 1
+                final_nickname = "#{nickname}_#{i}"
+              end
+              final_nickname
+            end
+            let(:body) do
+              {
+                grant_type: "password",
+                auth_type: "impersonate",
+                username: nickname,
+                client_id: api_client.client_id,
+                client_secret: api_client.client_secret,
+                meta: {
+                  register_on_missing: false
+                },
+                scope: "public"
+              }
+            end
+
+            run_test!(example_name: :user_not_found) do |_example|
+              expect(Decidim::User.find_by(nickname: nickname)).to be_nil
+            end
+          end
+
+          context "with meta.register_on_missing=true and invalid username" do
+            let(:nickname) do
+              final_nickname = nickname = "d"
+              i = 0
+              while Decidim::User.find_by(nickname: final_nickname)
+                i += 1
+                final_nickname = "#{nickname}_#{i}"
+              end
+              final_nickname
+            end
+            let(:body) do
+              {
+                grant_type: "password",
+                auth_type: "impersonate",
+                username: nickname,
+                client_id: api_client.client_id,
+                client_secret: api_client.client_secret,
+                meta: {
+                  register_on_missing: false
+                },
+                scope: "public"
+              }
+            end
+
+            run_test!(example_name: :invalid_username_on_register) do |_example|
+              expect(Decidim::User.find_by(nickname: nickname)).to be_nil
+            end
+          end
+        end
+
+        context "with client_id from another organization" do
+          let(:organization_b) { create(:organization) }
+          let(:foreign_api_client) { create(:api_client, organization: organization_b) }
+          let(:body) do
+            {
+              grant_type: "password",
+              username: user.nickname,
+              auth_type: "impersonate",
+              client_id: foreign_api_client.client_id,
+              client_secret: foreign_api_client.client_secret,
+              scope: "public"
+            }
+          end
+
+          before do
+            host! organization.host
+          end
+
+          run_test!(example_name: :bad_request)
+        end
+
+        context "with impersonate username from an another tenant" do
+          let(:other_tenant) { create(:organization) }
+          let(:body) do
+            {
+              grant_type: "password",
+              username: user.nickname,
+              auth_type: "impersonate",
+              client_id: api_client.client_id,
+              client_secret: api_client.client_secret,
+              scope: "public"
+            }
+          end
+
+          before { host! other_tenant.host }
+
+          run_test!
+        end
+
+        context "with scope=system and password grant" do
+          let(:system_api_client) { create(:api_client, organization: organization, scopes: "system") }
+          let(:body) do
+            {
+              grant_type: "password",
+              username: user.nickname,
+              auth_type: "impersonate",
+              client_id: system_api_client.client_id,
+              client_secret: system_api_client.client_secret,
+              scope: "system"
+            }
+          end
+
+          run_test!
+        end
+      end
+    end
+  end
+end
--- spec/decidim/rest_full/test/definitions/password_grant.rb
+++ spec/decidim/rest_full/test/definitions/password_grant.rb
@@ -25,6 +25,24 @@ module Api
         grant_type: { type: :string, enum: ["password"], description: "Resource Owner Password Credentials (ROPC) Flow, for **user impersonation**" },
         auth_type: { type: :string, enum: ["impersonate"], description: "Type of ROPC" },
         username: { type: :string, description: "User nickname" },
+        extra: {
+          type: :object,
+          title: "User extra data",
+          description: "Additional properties for the user (private)",
+          properties: {
+            email: { type: :string, description: "User email. Default to <username>@example.org" }
+          }
+        },
+        meta: {
+          type: :object,
+          title: "User impersonation settings",
+          description: "Impersonation Settings",
+          properties: {
+            register_on_missing: { type: :boolean, description: "Register the user if it does not exists. Default: false" },
+            accept_tos_on_register: { type: :boolean, description: "Accept the TOS on registration, valid only if register_on_missing=true. Default: false" },
+            skip_confirmation_on_register: { type: :boolean, description: "Skip email confirmation on creation, valid only if register_on_missing=true. Default: false" }
+          }
+        },
         client_id: { type: :string, description: "OAuth application Client Id" },
         client_secret: { type: :string, description: "OAuth application Client Secret" },
         scope: { type: :string, enum: Doorkeeper.configuration.scopes.to_a.reject { |scope| scope == "system" }, description: "Request scopes" }
