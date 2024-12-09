d2fe0148a4a4de4fc25edb4f3a24fbe8cd9023ca
Hadrien Froger <hadrien@octree.ch>
2024-12-09 07:13:47 +0000

    :sparkles: (public) Show space details
    Add /:manifest/:id endpoint to show details of a given space. The return result is for now exactly the same schema as the /public/spaces index, and is queen to evolve.

--- app/controllers/decidim/api/rest_full/public/spaces_controller.rb
+++ app/controllers/decidim/api/rest_full/public/spaces_controller.rb
@@ -47,6 +47,37 @@ module Decidim
             ).serializable_hash
           end
 
+          def show
+            manifest_name = params.require(:manifest_name)
+            model_class_name = manifest_data(manifest_name)[:model]
+            raise Decidim::RestFull::ApiException::BadRequest, "manifest not supported: #{manifest_name}" unless Object.const_defined?(model_class_name)
+
+            model = model_class_name.constantize
+            select_transparent = if model.column_names.include? :is_transparent
+                                   "#{model.table_name}.is_transparent"
+                                 else
+                                   "FALSE as is_transparent"
+                                 end
+            query = model.where(organization: current_organization).visible_for(act_as).select(
+              "#{model.table_name}.created_at",
+              "#{model.table_name}.updated_at",
+              "#{model.table_name}.id",
+              "#{model.table_name}.title",
+              "#{model.table_name}.subtitle",
+              "#{model.table_name}.description",
+              "#{model.table_name}.short_description",
+              "#{model.table_name}.private_space",
+              select_transparent,
+              "'#{manifest_name}' AS manifest_name",
+              "'#{model_class_name}' AS class_name"
+            ).find(params.require(:id))
+
+            render json: SpaceSerializer.new(
+              query,
+              params: { only: [], locales: available_locales, host: current_organization.host }
+            ).serializable_hash
+          end
+
           private
 
           def space_manifest_names
@@ -69,7 +100,7 @@ module Decidim
             when :assemblies
               { model: "Decidim::Assembly", manifest: manifest }
             else
-              raise Decidim::RestFull::ApiException::BadRequest, "manifest not found: #{manifest}"
+              raise Decidim::RestFull::ApiException::BadRequest, "manifest not supported: #{manifest}"
             end
           end
         end
