0bbb0cb6c679cfcad550e047ba8e7a7a3ec39def
Hadrien Froger <hadrien@octree.ch>
2026-08-26 18:15:44 +0100

    chore: fix i18n-tasks config and missing translations

---
+++ decidim-restfull-core/spec/requests/decidim/rest_full/system/api_clients_controller_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe "Decidim RestFull system API clients admin pages" do
+  include Warden::Test::Helpers
+
+  let!(:organization) { create(:organization, available_locales: ["en"]) }
+  let!(:system_admin) { create(:admin) }
+  let!(:api_client) { create(:api_client, organization:, scopes: %w(system public)) }
+
+  before do
+    login_as system_admin, scope: :admin
+    host!(organization.host)
+  end
+
+  after { Warden.test_reset! }
+
+  it "renders index, new, show, and edit without strict locals errors" do
+    get Decidim::Core::Engine.routes.url_helpers.system_api_clients_path
+    expect(response).to have_http_status(:ok)
+
+    get Decidim::Core::Engine.routes.url_helpers.new_system_api_client_path
+    expect(response).to have_http_status(:ok)
+
+    get Decidim::Core::Engine.routes.url_helpers.system_api_client_path(api_client)
+    expect(response).to have_http_status(:ok)
+
+    get Decidim::Core::Engine.routes.url_helpers.edit_system_api_client_path(api_client)
+    expect(response).to have_http_status(:ok)
+  end
+end
